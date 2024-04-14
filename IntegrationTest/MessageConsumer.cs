f899473070a1121bb2ed7dfe519800ab2b369fda
quantum-booty <henrywang97@gmail.com>
2024-04-14 13:12:02 +0100

    log message rate

--- IntegrationTest/MessageConsumer.cs
+++ IntegrationTest/MessageConsumer.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Threading.Tasks;
 using MassTransit;
 using Microsoft.Extensions.Logging;
 using Protocol;
 
 namespace IntegrationTest;
+
+public static class Counter
+{
+    public static int Count { get; set; }
+    public static readonly DateTime StartTime = DateTime.UtcNow;
+}
 
 public partial class MessageConsumer : IConsumer<Message>
 {
@@ -13,11 +20,14 @@ public partial class MessageConsumer : IConsumer<Message>
 
     public Task Consume(ConsumeContext<Message> context)
     {
-        LogReceived(_logger, context.Message.Text);
+        var secondPassed = (DateTime.UtcNow - Counter.StartTime).TotalSeconds;
+        var messagePerSec = Counter.Count / secondPassed;
+        LogReceived(_logger, context.Message.Text, Counter.Count, secondPassed, messagePerSec);
+        Counter.Count += 1;
 
         return Task.CompletedTask;
     }
 
-    [LoggerMessage(1, LogLevel.Information, "Received Text: {Text}")]
-    private static partial void LogReceived(ILogger logger, string text);
+    [LoggerMessage(1, LogLevel.Information, "Received Text: {Text} {count} {secondPassed} {messagePerSec}")]
+    private static partial void LogReceived(ILogger logger, string text, int count, double secondPassed, double messagePerSec);
 }
